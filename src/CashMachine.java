2d8ca1bc58cd2d46c90c3da024c55b928f193e27
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-04-10 17:14:11 +0300

    Создать класс описывающий Банкомат.
    Набор купюр, находящихся в банкомате, должен задаваться свойствами: количеством купюр номиналом 20, 50, 100.
    Сделать методы для добавления денег в банкомат.
    Сделать метод, снимающий деньги.
    С клавиатуры передается сумма денег. На экран выводим операция удалась или нет.
    При снятии денег метод должен выводить на экран каким количеством купюр и какого номинала выдается сумма.
    Создать конструктор с тремя параметрами – количеством купюр разного наминала.

--- src/CashMachine.java
+++ src/CashMachine.java
@@ -1,2 +1,93 @@
-package PACKAGE_NAME;public class CashMachine {
+import java.util.Scanner;
+
+public class CashMachine {
+    private int num20;
+    private int num50;
+    private int num100;
+
+    public CashMachine(int num20, int num50, int num100) {
+        this.num20 = num20;
+        this.num50 = num50;
+        this.num100 = num100;
+    }
+
+    public void setMoney() {
+        Scanner scan2 = new Scanner(System.in);
+        int sum, remain;
+        do {
+            System.out.println("Введите количество денег. Введите ноль для выхода в основное меню");
+            sum = scan2.nextInt();
+            if (sum == 0) {
+                break;
+            } else {
+                num100 = num100 + sum / 100;
+                num50 = num50 + (sum - sum / 100 * 100) / 50;
+                num20 = num20 + (((sum - sum / 100 * 100) % 50) / 20);
+                remain = ((sum % 100) % 50) % 20;
+                if (remain != 0) {
+                    System.out.printf("Не удалось внести часть денег в количестве %d\n", remain);
+                } else {
+                    System.out.println("Сумма внесена в банкомат");
+                }
+                sum = 0;
+            }
+        } while (sum != 0);
+    }
+
+    public void getMoney() {
+        Scanner scan3 = new Scanner(System.in);
+        int sum, cash = 100 * num100 + 50 * num50 + 20 * num20;
+        do {
+            System.out.printf("""
+                    Доступно купюр номиналом 100 - %d
+                    Доступно купюр номиналом 50 - %d
+                    Доступно купюр номиналом 20 - %d
+                    """, num100, num50, num20);
+            System.out.println("Введите количество денег для получения. Введите ноль для выхода в основное меню");
+            sum = scan3.nextInt();
+            if (sum == 0) {
+                break;
+            } else {
+                // расчитываем количество купюр для выдачи
+                if (sum < cash) {
+                    if (sum >= num100 * 100) {
+                        sum -= num100 * 100;
+                        System.out.println("Выдано купюр номиналом 100 - " + num100);
+                        num100 = 0;
+                    } else {
+                        num100 -= sum / 100;
+                        System.out.println("Выдано купюр номиналом 100 - " + sum / 100);
+                        sum -= sum / 100 * 100;
+                    }
+
+                    if (sum >= num50 * 50) {
+                        sum -= num50 * 50;
+                        System.out.println("Выдано купюр номиналом 50 - " + num50);
+                        num50 = 0;
+                    } else {
+                        num50 -= sum / 50;
+                        System.out.println("Выдано купюр номиналом 50 - " + sum / 50);
+                        sum -= sum / 50 * 50;
+                    }
+
+                    if (sum >= num20 * 20) {
+                        sum -= num20 * 20;
+                        System.out.println("Выдано купюр номиналом 20 - " + num20);
+                        num20 = 0;
+                    } else {
+                        num20 -= sum / 20;
+                        System.out.println("Выдано купюр номиналом 20 - " + sum / 20);
+                        sum -= sum / 20 * 20;
+                    }
+                    System.out.println("Получите деньги");
+                    if (sum != 0) {
+                        System.out.printf("Не удалось выдать количество денег %d\n", sum);
+                    }
+                } else {
+                    System.out.println("В банкомате недостаточно денег");
+                    sum = 0;
+                }
+            }
+        } while (sum != 0);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan1 = new Scanner(System.in);
+        char c;
+        CashMachine cashMachine = new CashMachine(10, 10, 10);
+        do {
+            System.out.println("Введите команду. Для вывода списка команд введите i. Для завершения работы введите q");
+            c = scan1.next().charAt(0);
+            switch (c) {
+                case 'q' -> System.out.println("Завершение работы");
+                case 's' -> cashMachine.setMoney();
+                case 'g' -> cashMachine.getMoney();
+                case 'i' -> System.out.println("""
+                        Список команд
+                        s - снять деньги
+                        g - внести деньги
+                        i - список команд
+                        q - завершение работы""");
+                default -> System.out.println("Неизвестная команда");
+            }
+        } while (c != 'q');
+        scan1.close();
     }
 }
